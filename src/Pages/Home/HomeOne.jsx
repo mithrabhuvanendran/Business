c8d88a072478ff3019562f83ad21bf893945098b
mithra <mithra2494@gmail.com>
2025-11-11 10:15:52 +0530

    fourth commit

--- src/Pages/Home/HomeOne.jsx
+++ src/Pages/Home/HomeOne.jsx
@@ -1,25 +1,42 @@
 import { CiPlay1 } from "react-icons/ci";
-import HomeImg from "../../assets/home.jpg"
+import HomeImg from "../../assets/vitaly-gariev-_4tpElFQemQ-unsplash.jpg";
 
 const HomeOne = () => {
   return (
-    <section id='home' className='grid place-items-center p-5! bg-blue-600 text-white'>
-        <figure className='flex flex-col items-center justify-center gap-8 w-[80vw] sm:w-[85vw] md:w-[90vw] md:flex-row lg:w-[85vw] xl:w-[90vw] 2xl:w-5/6 h-150 sm:h-180 md:h-150'>
-            <figcaption className="flex flex-col gap-8">
-                <h1 className="font-bold text-2xl w-full sm:text-4xl md:text-3xl lg:text-4xl md:w-full xl:text-5xl xl:w-5/6 lg:leading-10 xl:leading-15 ">Corporate & Business Site Template By Ayro UI.</h1>
-                <p className="text-zinc-300">We are a digital agency that helps brands to achieve their business outcomes. We see technology as a tool to create amazing things. </p>
-                <div className="flex items-center gap-3 sm:gap-5">
-                  <button className="border text-[12px] sm:text-base px-5! md:px-5! lg:px-7! py-3! font-bold bg-white text-blue-600 rounded-lg hover:border-white hover:bg-blue-600 hover:text-white cursor-pointer">GET STARTED</button>
-                  <button className="border p-2! sm:p-3! rounded-full bg-white text-blue-600 cursor-pointer"><CiPlay1 /></button>
-                  <label htmlFor="" className="cursor-pointer font-bold text-[14px] sm:text-base">Watch Intro</label>
-                </div>
-            </figcaption>
-            <div className="sm:w-[85vw] md:w-250 sm:h-85 md:h-100 lg:h-120 ">
-                <img className="rounded-lg" src={HomeImg} alt="" />
-            </div>
-        </figure>
+    <section
+      id="home"
+      className="grid place-items-center p-5! bg-blue-600 text-white"
+    >
+      <figure className="flex flex-col items-center justify-center gap-8 w-[80vw] sm:w-[85vw] md:w-[90vw] md:flex-row lg:w-[85vw] xl:w-[90vw] 2xl:w-5/6 h-150 sm:h-180 md:h-150">
+        <figcaption className="flex flex-col gap-8">
+          <h1 className="font-bold text-2xl w-full sm:text-4xl md:text-3xl lg:text-4xl md:w-full xl:text-5xl xl:w-5/6 lg:leading-10 xl:leading-15 ">
+            Corporate & Business Site Template By Ayro UI.
+          </h1>
+          <p className="text-zinc-300">
+            We are a digital agency that helps brands to achieve their business
+            outcomes. We see technology as a tool to create amazing things.{" "}
+          </p>
+          <div className="flex items-center gap-3 sm:gap-5">
+            <button className="border text-[12px] sm:text-base px-5! md:px-5! lg:px-7! py-3! font-bold bg-white text-blue-600 rounded-lg hover:border-white hover:bg-blue-600 hover:text-white cursor-pointer">
+              GET STARTED
+            </button>
+            <button className="border p-2! sm:p-3! rounded-full bg-white text-blue-600 cursor-pointer">
+              <CiPlay1 />
+            </button>
+            <label
+              htmlFor=""
+              className="cursor-pointer font-bold text-[14px] sm:text-base"
+            >
+              Watch Intro
+            </label>
+          </div>
+        </figcaption>
+        <div className="sm:w-[85vw] md:w-250 sm:h-85 md:h-100 lg:h-120 ">
+          <img className="rounded-lg" src={HomeImg} alt="" />
+        </div>
+      </figure>
     </section>
-  )
-}
+  );
+};
 
-export default HomeOne
+export default HomeOne;
--- src/Pages/LatestNews/LatestNewsCard.jsx
+++ src/Pages/LatestNews/LatestNewsCard.jsx
@@ -6,19 +6,19 @@ const latestNews = [
         id: 1,
         name: "Make your team a Design driven company",
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.",
-        img: "https://picsum.photos/500/300?random=30"
+        img: "https://res.cloudinary.com/ddfhe3fan/image/upload/v1762775446/ux-indonesia-pqzRfBhd9r0-unsplash_nzxsms.jpg"
     },
     {
         id: 2,
         name: "The newest web framework that changed the world",
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.",
-        img: "https://picsum.photos/500/300?random=31"
+        img: "https://res.cloudinary.com/ddfhe3fan/image/upload/v1762775450/tom-podmore-TwEhgfCWISA-unsplash_kxnvof.jpg"
     },
     {
         id: 3,
         name: "5 ways to improve user retention for your startup",
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.",
-        img: "https://picsum.photos/500/300?random=32"
+        img: "https://res.cloudinary.com/ddfhe3fan/image/upload/v1762775479/ux-indonesia-w00FkE6e8zE-unsplash_1_gbchoh.jpg"
     },
 ]
 
